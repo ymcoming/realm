ecbaffacdd51605f20f7094b9e1e88e3eba8dfe8
ymcoming <48167549+ymcoming@users.noreply.github.com>
2021-05-03 17:24:46 +0800

    Delete realm.sh

--- realm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /root
-wget https://github.com.cnpmjs.org/ymcoming/realm/releases/download/V1.2.1/realm.zip && unzip realm.zip && chmod +x ./realm && mkdir /etc/realm && mv realm /etc/realm && mv config.json /etc/realm && mv realm.service  /etc/systemd/system && systemctl enable --now realm && systemctl start realm && systemctl status realm
